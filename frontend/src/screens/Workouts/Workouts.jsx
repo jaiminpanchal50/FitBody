66a9c8fb44522024c11b7f72ed12a14645cf9c0e
Jaimin <jaiminp.thoughtmate@gmail.com>
2025-12-27 18:25:21 +0530

    add Home screen with workout selection and details

--- frontend/src/screens/Workouts/Workouts.jsx
+++ frontend/src/screens/Workouts/Workouts.jsx
@@ -4,7 +4,7 @@ import Banner from '../../components/Banner/Banner'
 const Workouts = () => {
     return (
         <>
-           <Banner title="Your fitness journey is a reflection of your discipline and dedication" description="Track every workout, measure your progress, and build sustainable habits that lead to long-term strength and confidence." btnText={'learn more'} link={'/   '} />
+           <Banner title="Your fitness journey is a reflection of your discipline and dedication" description="Track every workout, measure your progress, and build sustainable habits that lead to long-term strength and confidence." btnText={'learn more'} link={'/'} />
         </>
     )
 }
